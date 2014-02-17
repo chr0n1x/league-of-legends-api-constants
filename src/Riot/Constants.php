925d8fc5ead640d7c597c12164d47347b745c62e
chr0n1x <heilong24@gmail.com>
2014-02-17 00:49:05 -0500

    composer.json + constants class with regions

---
+++ src/Riot/Constants.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Riot;
+
+class Constants {
+
+  public function getRegions() {
+
+    return array(
+        'Brazil' => 'br',
+        'EU Nordic & East' => 'eune',
+        'EU West' => 'euw',
+        'Korea' => 'kr',
+        'Latin America North' => 'lan',
+        'Latin American South' => 'las',
+        'North America' => 'na',
+        'Oceanic' => 'oce',
+        'Russia' => 'ru',
+        'Turkey' => 'tr'
+    );
+
+  } // getRegions
+
+} // Constants
